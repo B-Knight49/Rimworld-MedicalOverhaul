7a8a2d35e34e3f5eec7633caa2a79fb33de45a1d
Scott Crawley <B-Knight49>
2018-07-11 21:32:17 +0100

    0Harmony is now being used to patch the BleedRate modifier

--- Source/MedicalOverhaul/MedicalOverhaul/Bypass.cs
+++ Source/MedicalOverhaul/MedicalOverhaul/Bypass.cs
@@ -12,6 +12,25 @@ using IV;
 using System.Collections;
 using System.Reflection;
 
+[StaticConstructorOnStartup]
+class Main
+{
+    static Main()
+    {
+        var harmony = HarmonyInstance.Create("com.medical.iv");
+        harmony.PatchAll(Assembly.GetExecutingAssembly());
+    }
+}
+
+[HarmonyPatch(typeof(HediffSet), "CalculateBleedRate")]
+static class HarmonyPatches
+{
+    static void Postfix(ref float __result, HediffSet __instance)
+    {
+        __result *= __instance.pawn.GetStatValue(StatDef.Named("BleedRate"));
+    }
+}
+
 namespace IV
 {
     public class Building_Bypass : Building
