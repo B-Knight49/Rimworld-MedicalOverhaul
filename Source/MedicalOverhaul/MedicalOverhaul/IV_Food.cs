5a2abb08ae3e4c1951569d4704ebb047bb817be2
Scott Crawley <B-Knight49>
2018-07-13 14:27:34 +0100

    Merging Medical IV's into the mod.

---
+++ Source/MedicalOverhaul/MedicalOverhaul/IV_Food.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RimWorld;
+using Verse;
+using Verse.AI;
+using Verse.Sound;
+using Harmony;
+using Harmony.ILCopying;
+using IV;
+using System.Collections;
+using System.Reflection;
+
+namespace IV
+{
+    public class Building_FoodMachine : Building
+    {
+
+        public static HediffDef IV_Food = HediffDef.Named("IV_Food");
+        private CompPowerTrader powerComp = null;
+
+        public Building_FoodMachine()
+            : base()
+        {
+            Log.Message("[Medical IV's] Initialised Nutrition Machine");
+        }
+
+        // Make sure it has power before doing stuff
+        public override void SpawnSetup(Map map, bool respawningAfterLoad)
+        {
+            base.SpawnSetup(map, respawningAfterLoad);
+            powerComp = base.GetComp<CompPowerTrader>();
+        }
+
+        // Repeat code every Tick (might change it to TickRare at some point)
+        public override void Tick()
+        {
+
+            if ((Find.TickManager.TicksGame + thingIDNumber.HashOffset()) % 60 == 0)
+            {
+                base.Tick();
+
+                if (powerComp.PowerOn)
+                {
+                    ApplyIV();
+                }
+            }
+        }
+
+        // Apply Hediff to every Pawn in adjacent cells once Tick() has been called
+        public void ApplyIV()
+        {
+            var adjacent = GenAdj.CardinalDirectionsAround;
+            var position = this.Position;
+            for (int i = 0; i < adjacent.Length; i++)
+            {
+                var things = this.Map.thingGrid.ThingsListAt(adjacent[i] + position);
+                foreach (Thing thing in things)
+                {
+                    if (thing is Pawn)
+                    {
+                        Pawn pawn = thing as Pawn;
+                        pawn.health.AddHediff(IV_Food);
+                        pawn.needs.food.ForceSetLevel(1f);
+
+                    }
+
+                }
+            }
+        }
+
+    }
+}
+
