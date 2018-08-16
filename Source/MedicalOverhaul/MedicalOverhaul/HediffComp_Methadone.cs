d253b059df93e2441052ce3cb611c6dd1c644aa7
Scott Crawley <B-Knight49>
2018-08-16 21:28:52 +0100

    Fixed issue #55 // Begun work on Methadone and added temp artwork

---
+++ Source/MedicalOverhaul/MedicalOverhaul/HediffComp_Methadone.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RimWorld;
+using Verse;
+using Verse.AI;
+using Verse.Sound;
+using Harmony;
+using Harmony.ILCopying;
+using IV;
+using System.Collections;
+using System.Reflection;
+
+namespace IV
+{
+    public class HediffComp_Methadone : HediffComp
+    {
+
+        public HediffCompProperties_Methadone Props
+        {
+            get
+            {
+                return (HediffCompProperties_Methadone)base.props;
+            }
+        }
+
+        public override void CompPostMake()
+        {
+            List<Hediff> Hediffs = base.Pawn.health.hediffSet.GetHediffs<Hediff>().ToList();
+
+            foreach (Hediff hediff in Hediffs)
+            {
+                // Opiate's below:
+                // - Flake 
+                // - Yayo (THIS IS A STIMULANT, NOT AN OPIATE. MIGHT ADD AT FUTURE DATE)
+                // - GoJuice
+                // - WakeUp
+
+                var StrHediff = hediff.ToString();
+                if (StrHediff.Contains("Flake") || StrHediff.Contains("GoJuice") || StrHediff.Contains("WakeUp"))
+                {
+                    base.Pawn.health.RemoveHediff(hediff);
+                    Log.Message("Removed hediff: " + hediff);
+                }
+            }
+        }
+    }
+}
